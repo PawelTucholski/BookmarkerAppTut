92ff01187f2da62a9927842e2452a706ae932c5c
Pawel <sorryej@gmail.com>
2018-02-25 12:12:13 +0100

    complete Bookmarker App

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ function saveBookmark (e) {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
 
-  if (!siteName || !siteUrl) {
-    alert("Please fill in the form");
+  if(!validateForm(siteName, siteUrl)) {
     return false;
   }
 
@@ -24,6 +23,8 @@ function saveBookmark (e) {
     bookmarks.push(bookmark);
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
+  document.getElementById('myForm').reset();
+
   fetchBookmarks();
 
   e.preventDefault();
@@ -56,4 +57,19 @@ function fetchBookmarks() {
                                   '</h3>'+
                                   '</div>';
   }
+}
+function validateForm(siteName, siteUrl) {
+  if (!siteName || !siteUrl) {
+    alert("Please fill in the form");
+    return false;
+  }
+
+  var expression =/[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert("Please use a valid URL");
+    return false;
+  }
+  return true;
 }
