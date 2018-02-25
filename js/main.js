362b3dd1508160d0120fd982f4137b50e2cdba7f
Pawel <sorryej@gmail.com>
2018-02-25 11:26:57 +0100

    added save bookmark

--- js/main.js
+++ js/main.js
@@ -9,6 +9,15 @@ function saveBookmark (e) {
     url: siteUrl,
   };
 
-  
+  if (localStorage.getItem('bookmarks') === null) {
+    var bookmarks[];
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  } else {
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+  }
+
   e.preventDefault();
 };
