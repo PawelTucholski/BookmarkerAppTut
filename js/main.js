7cf8e47a18371a65df9401ac357ac5492ecaa8a7
Pawel <sorryej@gmail.com>
2018-02-25 11:59:10 +0100

    fixed saveBookmark() and added deleteBookmark and fetchBookmark()

--- js/main.js
+++ js/main.js
@@ -4,13 +4,19 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 function saveBookmark (e) {
   var siteName = document.getElementById('siteName').value;
   var siteUrl = document.getElementById('siteUrl').value;
+
+  if (!siteName || !siteUrl) {
+    alert("Please fill in the form");
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl,
   };
 
   if (localStorage.getItem('bookmarks') === null) {
-    var bookmarks[];
+    var bookmarks = [];
     bookmarks.push(bookmark);
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   } else {
@@ -18,6 +24,36 @@ function saveBookmark (e) {
     bookmarks.push(bookmark);
     localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   }
+  fetchBookmarks();
 
   e.preventDefault();
 };
+
+function deleteBookmark(url){
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].url == url) {
+      bookmarks.splice(i, 1);
+    }
+  }
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  fetchBookmarks();
+}
+
+function fetchBookmarks() {
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  var bookmarkResults = document.getElementById('bookmarkResults');
+  bookmarkResults.innerHTML = '';
+  for (var i = 0; i < bookmarks.length; i++) {
+    var name = bookmarks[i].name;
+    var url = bookmarks[i].url;
+    bookmarkResults.innerHTML += '<div class="well">'+
+                                  '<h3>'+name+
+                                  '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+                                  '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
+                                  '</h3>'+
+                                  '</div>';
+  }
+}
